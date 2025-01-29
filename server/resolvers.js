a039eeef14d3a6a3fc3e399a8257be8556ca6f27
Matheus Alves <matheusalves.bmd@gmail.com>
2025-01-29 04:23:50 -0300

    Play with arrays and non-nullability

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,9 +1,18 @@
 export const resolvers = {
   Query: {
-    job: () => ({
-      id: "job-id",
-      title: "job title",
-      description: "job description",
-    }),
+    jobs: () => [
+      {
+        id: "job-id-1",
+        title: "job title 1",
+        description: "job description",
+      },
+      {
+        id: "job-id-2",
+        title: "job title 2",
+        description: "job description",
+      },
+      // null, // Would cause an error
+      // An empty array is perfectly acceptable, tough.
+    ],
   },
 };
