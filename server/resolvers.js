f851498a6038f4a5f1b8eddc5785e532fefae3a3
Matheus Alves <matheusalves.bmd@gmail.com>
2025-02-03 03:00:42 -0300

    Retrieve jobs from the database

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,18 +1,7 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    jobs: () => [
-      {
-        id: "job-id-1",
-        title: "job title 1",
-        description: "job description",
-      },
-      {
-        id: "job-id-2",
-        title: "job title 2",
-        description: "job description",
-      },
-      // null, // Would cause an error
-      // An empty array is perfectly acceptable, tough.
-    ],
+    jobs: () => getJobs(),
   },
 };
