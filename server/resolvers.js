bf24caca0c9cdfdd7ca8685ae2722d9934715239
Matheus Alves <matheusalves.bmd@gmail.com>
2025-02-26 21:18:08 -0300

    Assign getJobs directly to jobs
    Since `getJobs` is a function that returns a Promise you can assign
    it directly to the `jobs` resolver.
    
    It keeps the code cleaner and avoids unnecessary function calls.

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,6 +2,7 @@ import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
-    jobs: () => getJobs(),
+    jobs: getJobs, // Feasible because getJobs is already a function that returns a value (or a Promise)
+    // jobs: () => getJobs(),
   },
 };
