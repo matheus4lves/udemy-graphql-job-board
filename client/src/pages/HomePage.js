f766421f7c20b929a65c91c6191c9423b1931968
Matheus Alves <matheusalves.bmd@gmail.com>
2025-03-12 05:32:33 -0300

    Display real jobs

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,25 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import { useEffect, useState } from "react";
+import JobList from "../components/JobList";
+import getJobs from "../graphql/queries.js";
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then(setJobs);
+
+    // Alternatively...
+    // async function fetchData() {
+    //   const jobs = await getJobs();
+    //   setJobs(jobs);
+    // }
+
+    // fetchData();
+  }, []);
+
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
